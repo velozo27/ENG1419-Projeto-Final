2689083413f5b3acf7f71641cb4ad3bd146aad50
Thiago Cerqueira <thicerqs@gmail.com>
2022-06-23 17:41:44 -0300

    Manda foto no Telegram

--- Telegram.py
+++ Telegram.py
@@ -5,9 +5,14 @@ id_da_conversa = "1143595271"
 base = "https://api.telegram.org/bot" + chave
 endereco = base + "/sendMessage"
 
-def mandar_aviso():
+def mandar_aviso(path_foto):
+    endereco = base + "/sendMessage"
     dados = {"chat_id": id_da_conversa, "text": "Movimento na camera"}
     post(endereco, json=dados)
     
+    endereco = base + "/sendPhoto"
+    arquivo = {"photo": open(path_foto, "rb")}
+    post(endereco, data=dados, files=arquivo)
+    
 
-mandar_aviso()
+#mandar_aviso("imagem.jpg")
